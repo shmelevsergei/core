359a840c1da1ce91bdb80b4871486ca8c6df4907
shmelevsergei <shmelevsergei86@gmail.com>
2024-01-11 15:03:33 +0300

    11.01.2024

---
+++ src/app/administrator/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <main className={'min-h-screen'}>
+            Administrator
+        </main>
+    );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+import Link from "next/link";
+
 import { PrismaClient } from '@prisma/client'
 
 const client = new PrismaClient()
-
 export default async function Home() {
 
-  const dats = await client.gA_ANKETA_BASE.findMany();
+    const dats = await client.gA_ANKETA_BASE.findMany();
 
-  console.log(dats[1])
+    console.log(dats[1])
 
-  return (
+
+    return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/*<Link href={'/administrator'}>ID: {dats}</Link>*/}
+        <ul>
+            {
+                dats.map(data => (
 
+                    <li key={data.ABA_ID}>
+                        <p>ID: {data.ABA_ID}</p>
+                        <p>INN: {data.ABA_INN}</p>
+                        <p>PHONE: {data.ABA_PHONE}</p>
+                    </li>
+                ))
+            }
+        </ul>
     </main>
   )
 }
