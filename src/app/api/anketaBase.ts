7885c8ff492f574ecee17e52a8979fcde00f862b
shmelevsergei <shmelevsergei86@gmail.com>
2024-01-16 02:29:05 +0300

    16.01.2024

--- src/app/api/anketaBase.ts
+++ src/app/api/anketaBase.ts
@@ -1,19 +1,19 @@
 
-import { PrismaClient } from '@prisma/client';
-import {NextApiRequest, NextApiResponse} from "next";
-
-const prisma = new PrismaClient();
-
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    if (req.method === 'GET') {
-        try {
-            const data = await prisma.gA_ANKETA_BASE.findMany();
-            res.status(200).json(data);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    } else {
-        res.status(405).json({ error: 'Method Not Allowed' });
-    }
-}
+// import { PrismaClient } from '@prisma/client';
+// import {NextApiRequest, NextApiResponse} from "next";
+//
+// const prisma = new PrismaClient();
+//
+// export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+//     if (req.method === 'GET') {
+//         try {
+//             const data = await prisma.gA_ANKETA_BASE.findMany();
+//             res.status(200).json(data);
+//         } catch (error) {
+//             console.error("Error fetching data:", error);
+//             res.status(500).json({ error: 'Internal Server Error' });
+//         }
+//     } else {
+//         res.status(405).json({ error: 'Method Not Allowed' });
+//     }
+// }
